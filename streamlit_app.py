93ef787162789ac4e9cead65e7bf85c79cbe747b
katrinhallikmaa <60192901+katrinhallikmaa@users.noreply.github.com>
2025-04-22 15:01:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,4 +57,4 @@ if time_to_insert and ingredients_string:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
